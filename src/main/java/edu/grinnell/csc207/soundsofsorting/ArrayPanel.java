3a8da76812dd51fe9a97235ca856b43bc625c296
Sydney Fraga <sjfraga24@gmail.com>
2025-12-19 08:26:31 -0600

    Submision

--- src/main/java/edu/grinnell/csc207/soundsofsorting/ArrayPanel.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/ArrayPanel.java
@@ -1,7 +1,6 @@
 package edu.grinnell.csc207.soundsofsorting;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.lang.reflect.Array;
 
 import javax.swing.JPanel;
 
@@ -34,8 +33,8 @@ public class ArrayPanel extends JPanel {
         int[] idxs = new int[3];
         int highLightCount = 0;
         int max = 390; 
+        g.clearRect(0, 0, width, max);
         if(notes.getNotes().length == 17){
-            g.clearRect(0, 0, width, max);
             for(int i = 0, j = 0; j < notes.getNotes().length; i+= barWidth, j++){
                 if(notes.isHighlighted(j)){
                     if(highLightCount == 1){
@@ -43,6 +42,8 @@ public class ArrayPanel extends JPanel {
                         g.drawRect(i, 0, barWidth, ControlPanel.bMinorPentatonicValues[(int)(notes.getNotes()[idxs[0]])]);
                         g.drawRect(idxs[1], 0, barWidth, ControlPanel.bMinorPentatonicValues[(int)(notes.getNotes()[idxs[2]])]);
                         highLightCount=0;
+                        notes.getNotes()[idxs[0]] = idxs[2];
+                        notes.getNotes()[idxs[2]] = idxs[0];
 
                     }else{
                         idxs[0] = j;
@@ -61,8 +62,31 @@ public class ArrayPanel extends JPanel {
                 }
             }
         }else{
-            for(int i = 0, j = 0; j < notes.getNotes().length; i+= barWidth, j++){
-                g.drawRect(i, 0, barWidth, ControlPanel.chromaticValues[(int)(notes.getNotes()[j])]);
+             for(int i = 0, j = 0; j < notes.getNotes().length; i+= barWidth, j++){
+                if(notes.isHighlighted(j)){
+                    if(highLightCount == 1){
+                        idxs[2] = j;
+                        g.drawRect(i, 0, barWidth, ControlPanel.chromaticValues[(int)(notes.getNotes()[idxs[0]])]);
+                        g.drawRect(idxs[1], 0, barWidth, ControlPanel.chromaticValues[(int)(notes.getNotes()[idxs[2]])]);
+                        highLightCount=0;
+                        notes.getNotes()[idxs[0]] = idxs[2];
+                        notes.getNotes()[idxs[2]] = idxs[0];
+
+                    }else{
+                        idxs[0] = j;
+                        idxs[1] = i;
+                        highLightCount++;
+                    }
+                }else{
+                    if(idxs[2] == 0){
+                        g.drawRect(i, 0, barWidth, ControlPanel.chromaticValues[(int)(notes.getNotes()[j])]);
+                    }else{
+                        if(j != idxs[0] && j!=idxs[2]){
+                            g.drawRect(i, 0, barWidth, ControlPanel.chromaticValues[(int)(notes.getNotes()[j])]);
+                        }
+                    }
+                    
+                }
             }
         }
         
